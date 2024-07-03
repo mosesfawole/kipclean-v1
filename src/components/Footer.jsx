a60220e852e03c1319fbaac7ccb426556b78405d
mosesfawle <mosesfawole2001@gmail.com>
2024-07-03 23:34:42 +0100

    fix major changes to the homepage both mobile and desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,17 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className=" bg-[#2f2f2f] text-white p-8 max-w-[1440px] mx-auto">
+    <div className=" bg-[#2f2f2f] text-white p-8 md:p-20 ">
       <div className="md:flex gap-4  items-baseline">
-        <div className=" ">
-          <h1 className=" mb-10 md:mb-0  text-3xl font-bold w-full sm:mr-8 md:mr-10  ">
+        <div className=" flex flex-1">
+          <h1 className=" mb-10 md:mb-0  text-3xl md:text-[50px] font-bold w-full sm:mr-8 md:mr-10  ">
             Kip-Clean
           </h1>
         </div>
         <div className="grid  grid-cols-2 sm:grid md:flex lg;grid md:mr-20  gap-16 justify-between ">
           <div className="order-2 md:order-1 w-full">
-            <h1 className="text-xl font-bold ">Support</h1>
-            <ul className="flex flex-col gap-4 pt-4 ">
+            <h1 className="text-xl font-bold md:text-3xl ">Support</h1>
+            <ul className="flex flex-col  gap-4 pt-4 ">
               <li>
                 <Link to={"/about"}>About Us</Link>
               </li>
@@ -34,19 +34,31 @@ const Footer = () => {
           </div>
           {/*  */}
           <div className="order-1 md:order-2 w-full ">
-            <h1 className="text-xl font-bold ">Services</h1>
-            <ul className="flex flex-col gap-4 pt-4 ">
-              <li>Regular Cleaning</li>
-              <li>Deep Cleaning</li>
-              <li>Office Cleaning</li>
-              <li>Plumbing Installation</li>
-              <li>Pipe Repair and Replacement</li>
-              <li>Water Heater Services</li>
+            <h1 className="text-xl font-bold md:text-3xl ">Services</h1>
+            <ul className="flex flex-col font-normal gap-4 pt-4 md:text-base">
+              <Link to="/Services">
+                <li>Regular Cleaning</li>
+              </Link>
+              <Link to="/Services">
+                <li>Deep Cleaning</li>
+              </Link>
+              <Link to="/Services">
+                <li>Office Cleaning</li>
+              </Link>
+              <Link to="/Services">
+                <li>Plumbing Installation</li>
+              </Link>
+              <Link to="/Services">
+                <li>Pipe Repair and Replacement</li>
+              </Link>
+              <Link to="/Services">
+                <li>Water Heater Services</li>
+              </Link>
             </ul>
           </div>
           {/*  */}
           <div className="order-3 w-full ">
-            <h1 className="text-xl font-bold ">Contact Us</h1>
+            <h1 className="text-xl font-bold md:text-3xl">Contact Us</h1>
             <ul className="flex flex-col gap-4 md:gap-9 pt-4 ">
               <li>No 2, Adewunmi layout,Oke Ayo,Ibadan,Oyo State.</li>
               <li>+2347013404146</li>
